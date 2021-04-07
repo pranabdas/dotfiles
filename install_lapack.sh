aeb910c59f9a9821242c2bcf851c8270f823be07
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2021-04-07 22:34:49 +0800

    Add files via upload

---
+++ install_lapack.sh
@@ -0,0 +1,21 @@
+# script to install lpack in ubuntu/debian
+apt update && apt upgrade -y
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends wget \
+    ca-certificates \
+    build-essential \
+    gfortran \
+    mpich \
+    fftw3-dev
+wget https://github.com/Reference-LAPACK/lapack/archive/refs/tags/v3.9.1.tar.gz
+tar -xzvf v3.9.1.tar.gz
+cd lapack-3.9.1
+cp make.inc.example make.inc
+make blaslib
+make lapacklib
+make tmglib
+cp librefblas.a /usr/local/lib/libblas.a
+cp liblapack.a /usr/local/lib/liblapack.a
+cp libtmglib.a /usr/local/lib/libtmg.a
+cd -
+rm v3.9.1.tar.gz
+rm -rf lapack-3.9.1
