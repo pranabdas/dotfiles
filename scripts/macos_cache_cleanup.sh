fee0e65d821d69c9e495fe1a2172cebd4d6af248
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2023-11-19 11:57:31 +0800

    update jammy dockerfile, macos cache cleanup script

--- scripts/macos_cache_cleanup.sh
+++ scripts/macos_cache_cleanup.sh
@@ -1,50 +1,55 @@
 sudo rm -rf /Library/Caches/*
 sudo rm -rf /Library/Logs/CrashReporter/CoreCapture/*
-rm /Library/Logs/DiagnosticReports/ch.protonvpn.mac.WireGuard*
 sudo rm -rf ~/Library/Caches/*
-rm -rf ~/Library/Caches/com.apple.ap.adprivacyd/*
-rm -rf ~/Library/Caches/com.apple.helpd/*
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/com.apple.desktoppicture/*
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/clang/*
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/com.apple.WebKit.WebContent/*
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/com.apple.Safari.SafeBrowsing/*
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/clang/*
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/com.apple.mediaanalysisd/*
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/com.seriflabs.affinityphoto
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/com.seriflabs.affinitydesigner
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/com.apple.WebKit.WebContent+org.webkit.Playwright
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/com.apple.WebKit.Networking+org.webkit.Playwright
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/com.apple.WebKit.GPU+org.webkit.Playwright
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/com.objective-see.KnockKnock
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/clang/*
-rm -rf /private/var/folders/nd/vqhrqncs08qdg2d5x4nfdmd40000gn/C/com.apple.Safari.SafeBrowsing/*
-rm -rf ~/Library/Application\ Support/Authy\ Desktop/Cache/*
-rm -rf ~/Library/Application\ Support/Code/Cache/*
-rm -rf ~/Library/Application\ Support/Code/CachedData/*
-rm -rf ~/Library/Application\ Support/EndNote/Updates/*
-rm -rf ~/Library/Application\ Support/Microsoft/Teams/tmp/*
-rm -rf ~/Library/Application\ Support/Signal/update-cache/*
-rm -rf ~/Library/Containers/desktop.WhatsApp/Data/Library/Application\ Support/WhatsApp/Cache/*
+sudo rm -rf ~/Library/Caches/com.apple.ap.adprivacyd/*
+sudo rm -rf ~/Library/Caches/com.apple.helpd/*
+sudo rm -rf /private/var/folders/*/*/C/com.apple.desktoppicture/*
+sudo rm -rf /private/var/folders/*/*/C/clang/*
+sudo rm -rf /private/var/folders/*/*/C/com.apple.WebKit.WebContent/*
+sudo rm -rf /private/var/folders/*/*/C/com.apple.Safari.SafeBrowsing/*
+sudo rm -rf /private/var/folders/*/*/C/com.apple.mediaanalysisd/*
+sudo rm -rf /private/var/folders/*/*/C/com.seriflabs.affinityphoto
+sudo rm -rf /private/var/folders/*/*/C/com.seriflabs.affinitydesigner
+sudo rm -rf /private/var/folders/*/*/C/com.apple.WebKit.WebContent+org.webkit.Playwright
+sudo rm -rf /private/var/folders/*/*/C/com.apple.WebKit.Networking+org.webkit.Playwright
+sudo rm -rf /private/var/folders/*/*/C/com.apple.WebKit.GPU+org.webkit.Playwright
+sudo rm -rf /private/var/folders/*/*/C/com.objective-see.KnockKnock
+sudo rm -rf ~/Library/Application\ Support/Caches/*
+sudo rm -rf ~/Library/Application\ Support/Authy\ Desktop/Cache/*
+sudo rm -rf ~/Library/Application\ Support/Code/Cache/*
+sudo rm -rf ~/Library/Application\ Support/Code/CachedData/*
+sudo rm -rf ~/Library/Application\ Support/Cypress/Cache/*
+sudo rm -rf ~/Library/Application\ Support/EndNote/Updates/*
+sudo rm -rf ~/Library/Application\ Support/Evernote/Cache/*
+sudo rm -rf ~/Library/Application\ Support/Microsoft/Teams/tmp/*
+sudo rm -rf ~/Library/Application\ Support/Signal/update-cache/*
+sudo rm -rf ~/Library/Application\ Support/Slack/Cache/*
+sudo rm -rf ~/Library/Containers/desktop.WhatsApp/Data/Library/Application\ Support/WhatsApp/Cache/*
 sudo rm -rf ~/Library/Containers/com.apple.Safari/Data/Library/Caches/CloudKit/*
-rm -rf ~/Library/Containers/com.apple.Safari/Data/Library/Caches/com.apple.Safari/WebKitCache/*
-rm -rf ~/Library/Containers/com.apple.AvatarUI.AvatarPickerMemojiPicker/Data/Library/Caches/com.apple.AvatarKit/*
-rm -rf ~/Library/Containers/com.apple.AppStore/Data/Library/Caches/com.apple.AppStore/*
-rm -rf ~/Library/Containers/com.apple.geod/Data/Library/Caches/com.apple.geod/*
-rm -rf ~/Library/Containers/com.apple.AMPArtworkAgent/Data/Library/Caches/com.apple.AMPArtworkAgent/*
-rm -rf ~/Library/Containers/com.apple.podcasts/Data/Library/Caches/com.apple.podcasts/*
-rm -rf ~/Library/Containers/com.microsoft.onenote.mac/Data/Library/Caches/Microsoft/*
-rm -rf ~/Library/Containers/com.microsoft.Word/Data/Library/Caches/Microsoft/*
-rm -rf ~/Library/Containers/com.apple.weather.widget/Data/Library/Caches/com.apple.weather.widget/*
-rm -rf ~/Library/Containers/com.apple.Notes/Data/Library/Caches/CloudKit/*
-rm -rf ~/Library/Containers/com.apple.mail/Data/Library/Caches/CloudKit/*
-rm -rf ~/Library/Containers/com.apple.iBooksX/Data/Library/Caches/*
+sudo rm -rf ~/Library/Containers/com.apple.Safari/Data/Library/Caches/com.apple.Safari/WebKitCache/*
+sudo rm -rf ~/Library/Containers/com.apple.AvatarUI.AvatarPickerMemojiPicker/Data/Library/Caches/com.apple.AvatarKit/*
+sudo rm -rf ~/Library/Containers/com.apple.AppStore/Data/Library/Caches/com.apple.AppStore/*
+sudo rm -rf ~/Library/Containers/com.apple.geod/Data/Library/Caches/com.apple.geod/*
+sudo rm -rf ~/Library/Containers/com.apple.AMPArtworkAgent/Data/Library/Caches/com.apple.AMPArtworkAgent/*
+sudo rm -rf ~/Library/Containers/com.apple.podcasts/Data/Library/Caches/com.apple.podcasts/*
+sudo rm -rf ~/Library/Containers/com.microsoft.onenote.mac/Data/Library/Caches/Microsoft/*
+sudo rm -rf ~/Library/Containers/com.microsoft.Word/Data/Library/Caches/Microsoft/*
+sudo rm -rf ~/Library/Containers/com.apple.weather.widget/Data/Library/Caches/com.apple.weather.widget/*
+sudo rm -rf ~/Library/Containers/com.apple.Notes/Data/Library/Caches/CloudKit/*
+sudo rm -rf ~/Library/Containers/com.apple.mail/Data/Library/Caches/CloudKit/*
+sudo rm -rf ~/Library/Containers/com.apple.iBooksX/Data/Library/Caches/*
+sudo rm -rf ~/Library/Containers/com.apple.weather/Data/Library/Caches/*
 sudo rm -rf ~/Library/Containers/com.apple.mail/Data/Library/Mail\ Downloads/*
-rm ~/Library/Preferences/org.webkit.Playwright.plist
-rm ~/Library/Preferences/com.objective-see.KnockKnock.plist
-rm ~/Library/Preferences/com.electron.docker-frontend.plist
-rm ~/Library/Preferences/org.darktable.plist
-rm ~/Library/Preferences/diagnostics_agent.plist
-rm -rf ~/Library/Saved\ Application\ State/com.electron.docker-frontend.savedState
-rm -rf ~/Library/Saved\ Application State/org.darktable.savedState
-npm cache clean --force
-yarn cache clean --force
+sudo rm -rf ~/Library/Saved\ Application\ State/com.electron.docker-frontend.savedState
+sudo rm -rf ~/Library/Saved\ Application\ State/org.darktable.savedState
+sudo rm ~/Library/Preferences/org.webkit.Playwright.plist
+sudo rm ~/Library/Preferences/com.objective-see.KnockKnock.plist
+sudo rm ~/Library/Preferences/com.electron.docker-frontend.plist
+sudo rm ~/Library/Preferences/org.darktable.plist
+sudo rm ~/Library/Preferences/diagnostics_agent.plist
+if command -v npm &> /dev/null; then
+    npm cache clean --force
+fi
+if command -v yarn &> /dev/null; then
+    yarn cache clean --force
+fi
